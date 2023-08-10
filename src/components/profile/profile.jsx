1dd6c708cfc38c7690991908a7dbf33f00ee7e12
cainhocr <caioignacio086@gmail.com>
2023-08-10 17:11:52 -0300

    finalizando aula 10/08

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile(){
+    return(
+    <div className="profile">
+<img src="" alt="" />
+        <p>@cr_cr_zn</p>
+    </div>
+    )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Profile from "../components/profile/profile";
+
+
+export default function app() {
+    return (
+        <>
+            <Profile></Profile>
+        </>
+    )
+};
