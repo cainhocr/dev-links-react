26499afd20cb44e7b547772a5e7eb030d83d898c
cainhocr <caioignacio086@gmail.com>
2023-08-11 14:54:56 -0300

    desenvolvido socialLinks

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
 import Profile from "../components/profile/profile";
-import Switch from "../components/switch/switch"
+import Switch from "../components/switch/switch";
 import Links from "../components/links/links";
+import SocialLinks from "../components/socialLinks/socialLinks";
 
 import "./App.css";
 
-import imgDarkMode from "../img/caiuuuu.jpg"
-import imgLightMode from "../img/cr.jpg"
+import imgDarkMode from "../img/caiuuuu.jpg";
+import imgLightMode from "../img/cr.jpg";
 
 const App = () => {
-    const [isLightMode,setIsLightMode] = useState(true)
+  const [isLightMode, setIsLightMode] = useState(true);
 
-const imgsrc = isLightMode ? imgLightMode :imgDarkMode;
-    const imgalt = isLightMode ? "imagem no Modo Claro" : "imagem no modo escuro"
+  const imgsrc = isLightMode ? imgLightMode : imgDarkMode;
+  const imgalt = isLightMode ? "imagem no Modo Claro" : "imagem no modo escuro";
   return (
     <div id="container">
-      <Profile imgsrc={imgDarkMode} imgalt={imgalt}>@cr_cr_zn</Profile>
+      <Profile imgsrc={imgDarkMode} imgalt={imgalt}>
+        @cr_cr_zn
+      </Profile>
       <Switch></Switch>
       <div id="links">
-
-      <ul>
-        <Links link={"https://github.com"}>Github</Links>
-        <Links link={"#"}>instagram</Links>
-        <Links link={"#"}>Portifolio</Links>
-        <Links link={"#"}>Projetos</Links>
-      </ul>
+        <ul>
+          <Links link={"https://github.com"}>Github</Links>
+          <Links link={"#"}>instagram</Links>
+          <Links link={"#"}>Portifolio</Links>
+          <Links link={"#"}>Projetos</Links>
+        </ul>
+      </div>
+      <div id="socialLinks">
+        <SocialLinks href={"#"} icon={"logo-github"} />
+        <SocialLinks href={"#"} icon={"logo-Instagram"} />
+        <SocialLinks href={"#"} icon={"logo-Youtube"} />
+        <SocialLinks href={"#"} icon={"logo-Twitter"} />
+        <SocialLinks href={"#"} icon={"logo-Facebook"} />
       </div>
     </div>
   );
