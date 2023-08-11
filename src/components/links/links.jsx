21737aa63a48533682748c86bca3b34d7a8af218
cainhocr <caioignacio086@gmail.com>
2023-08-11 13:59:33 -0300

    finalizada as partes dos links e iniciado o socialLinks

---
+++ src/components/links/links.jsx
@@ -0,0 +1,13 @@
+import styles from "./links.module.css";
+
+const Links = ({ children, link }) => {
+  return (
+    <li>
+      <a href={link} target="_blank">
+        {children}
+      </a>
+    </li>
+  );
+};
+
+export default Links;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Profile from "../components/profile/profile";
+import Switch from "../components/switch/switch"
+import Links from "../components/links/links";
 
 import "./App.css";
 
 import imgDarkMode from "../img/caiuuuu.jpg"
 import imgLightMode from "../img/cr.jpg"
-
 
 const App = () => {
     const [isLightMode,setIsLightMode] = useState(true)
@@ -13,8 +14,18 @@ const App = () => {
 const imgsrc = isLightMode ? imgLightMode :imgDarkMode;
     const imgalt = isLightMode ? "imagem no Modo Claro" : "imagem no modo escuro"
   return (
-    <div>
+    <div id="container">
       <Profile imgsrc={imgDarkMode} imgalt={imgalt}>@cr_cr_zn</Profile>
+      <Switch></Switch>
+      <div id="links">
+
+      <ul>
+        <Links link={"https://github.com"}>Github</Links>
+        <Links link={"#"}>instagram</Links>
+        <Links link={"#"}>Portifolio</Links>
+        <Links link={"#"}>Projetos</Links>
+      </ul>
+      </div>
     </div>
   );
 };
